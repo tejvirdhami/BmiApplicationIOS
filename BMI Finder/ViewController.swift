51af5c50c0d894629e79b1bf3a5d30ff67a39f8c
Tejvir Dhami <tejvirdhami99@gmail.com>
2021-09-03 13:39:56 -0400

    Initial Commit

---
+++ BMI Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Finder
+//
+//  Created by Tejvir  Dhami on 2021-09-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMI FinderTests/BMI_FinderTests.swift
@@ -0,0 +1,33 @@
+//
+//  BMI_FinderTests.swift
+//  BMI FinderTests
+//
+//  Created by Tejvir  Dhami on 2021-09-03.
+//
+
+import XCTest
+@testable import BMI_Finder
+
+class BMI_FinderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
