6427f2ab8ea3f7de6140894b236fa37eb0c18a2a
Tejvir Dhami <tejvirdhami99@gmail.com>
2021-09-09 23:11:54 -0400

    Final Version

--- BMI Finder/ViewController.swift
+++ BMI Finder/ViewController.swift
@@ -7,11 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var txtWeight: UITextField!
+    
+    
+    @IBOutlet var txtHeight: UITextField!
+    @IBOutlet var txtBMI: UITextField!
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+        
+        let stringWeight = txtWeight.text!
+        let weight = Float(stringWeight)
+        
+        let stringHeight = txtHeight.text!
+        let height = Float(stringHeight)
+        
+        var bmi: Float = 0
+        
+        var classi = ""
+        
+        bmi = Float(weight! / pow(height! , 2)) * 100 * 100
+        
+        bmi = round(bmi * 100) / 100
+        
+        let bmiString = String(bmi)
+        
+        switch bmi{
+        case 0..<18.5:
+        classi = "Underweight"
+        case 18.5..<24.9:
+        classi = "Normal weight"
+        case 25.0..<29.9:
+        classi = "Overweight"
+        case 30..<100.0:
+        classi = "Obesity"
+        default:
+        classi = "None"
+        }
+        
+        Toast.ok(view: self, title: "BMI", message: "\(bmiString)\n Classification: \(classi)", handler: nil)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        txtHeight.delegate = self
+        txtWeight.delegate = self
+    }
+    
+    func textFieldShouldReturn( _ textField: UITextField) ->Bool {
+        txtHeight.resignFirstResponder()
+        txtWeight.resignFirstResponder()
+        return true
     }
 
 
